778edaccfd88df1107daf3a347dc7f31f8ac76e1
Rashmi Rekha Gouda <129932828+Rashmi-Rekha@users.noreply.github.com>
2023-05-24 23:34:02 +0530

    Add files via upload
    Designed a web page by using html css and javascript

---
+++ design.js
@@ -0,0 +1,46 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginFrom.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let shoppingCart = document.querySelector('.shopping-cart');
+ 
+document.querySelector('#cart-btn').onclick = () =>{
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginFrom.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let loginFrom = document.querySelector('.login-form');
+ 
+document.querySelector('#login-btn').onclick = () =>{
+    loginFrom.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+    
+}
+
+let navbar = doument.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    loginFrom.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginFrom.classList.remove('active');
+    navbar.classList.remove('active');
+    
+}
+
+
